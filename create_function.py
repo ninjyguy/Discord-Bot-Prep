82d2ea8bfeb43b24d40346f2b415c7e5db62ef7a
Ilya Gotfryd <igotfryd@onshift.com>
2022-01-11 23:04:15 -0500

    Update create_function.py

--- create_function.py
+++ create_function.py
@@ -4,3 +4,4 @@
 # 4) wrap the whole code into a say_hello function and call it below\
 # 5) make @mention a parameter to a function and pass the string literal value in the call
 # 6) change print statement to a return value, return that value to a variable and print the result
+### CODE BELOW ###
