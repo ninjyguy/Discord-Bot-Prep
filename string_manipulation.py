618196641ec3b27ac0ca78cebaa7ff49ed058b5f
Ilya Gotfryd <igotfryd@onshift.com>
2022-01-11 22:47:15 -0500

    Update string_manipulation.py

--- string_manipulation.py
+++ string_manipulation.py
@@ -12,8 +12,8 @@ fifth_one = "One"
 # upper case a string
 # split a string to get individual words in it
 # find if string starts with "/chatbot"
-not_important = "This string is not important."
 very_important = "/chatbot This is very important to capture"
+not_important = "This string is not important."
 # find if a substring is IN a string
 good_one = "This string is very good for you."
 bad_one = "This string has no value at all."
